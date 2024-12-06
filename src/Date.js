b236a9d7a2f2a937c94447a49f99724aaad17cdf
aycakpinar-compe <ayca.akpnar@gmail.com>
2024-12-06 04:08:23 +0300

    Delete src directory

--- src/Date.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const CurrentDate = () => {
-  const currentDate = new Date().toLocaleString(); 
-
-  return <div>  {currentDate}</div>;
-};
-
-export default CurrentDate;
